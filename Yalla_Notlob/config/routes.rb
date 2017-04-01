50a299f29ee0054b0f81f4ab172dc921339765f6
HagerElsayed <hagerelsayed39@gmail.com>
2017-04-01 22:21:18 +0200

    BackUp

---
+++ Yalla_Notlob/config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+
+
+ resources :friendships
+
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  resources :friends
+  resources :users do
+      resources :orders do
+            resources :order_details
+        end
+      resources :groups
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "home#index"
+end
